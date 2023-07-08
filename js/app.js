e224bbab0d34e9a4b08a679704cc854215737691
Ivan <58911140+0xNord@users.noreply.github.com>
2023-07-08 18:15:32 +0300

    fixed bug (look description below)
    fixed bug when class .active added to any element inside buttons container.
    ---
    now it's apply only for buttons with certain class.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 let parent = document.getElementById('btns')
 console.log(parent)
 
-// buttons.onmouseup = function(){
-//     buttons.classList.toggle('active');
-// }
-// buttons.onmousedown = function(){
-//     buttons.classList.toggle('active');
-// }
-parent.addEventListener('mousedown', (e)=>{e.target.classList.toggle('active');});
-parent.addEventListener('mouseup', (e)=>{e.target.classList.toggle('active');});
+parent.addEventListener('mousedown', (e) => {
+    if (e.target.classList.contains("calculator__buttons-button")) { e.target.classList.toggle('active');}
+})
+parent.addEventListener('mouseup', (e) => {
+    if (e.target.classList.contains("calculator__buttons-button")) { e.target.classList.toggle('active');}
+})
+
